a8026b98fcc0caa08ab343191d032624cfa546a3
Tran Kevin <kevin.tran@etudiant.univ-rennes1.fr>
2022-06-06 23:49:31 +0200

    doc

--- include/matpol.h
+++ include/matpol.h
@@ -1,3 +1,15 @@
+/**
+ * \file mat_pol.h
+ * \brief shorts functions
+ * \author Kevin Tran
+ * \version 1.0
+ * \date June 6 2022
+ *
+ * Add some functions availables on SageMath for the class nmod_poly_mat_t on flint
+ * https://doc.sagemath.org/html/en/reference/matrices/sage/matrix/matrix_polynomial_dense.html
+ *
+ */
+
 #ifndef MATPOL_H
 #define MATPOL_H
 
@@ -6,6 +18,13 @@
 #include <nmod_poly_mat.h>
 
 
+/**
+ * \enum matrix_wise
+ * \brief way to see a polynomial matrix 
+ *
+ * Permits the user to use function in different perspectives 
+ *
+ */
 typedef enum
 {
     COLUMN_WISE = 0,
@@ -14,29 +33,134 @@ typedef enum
 } matrix_wise; 
 
 
-
+/**
+ * \fn void coefficient_matrix(nmod_mat_t res, const nmod_poly_mat_t mat, int degree)
+ * \brief Stock on res the coefficient matrix of degree degree of mat
+ * 
+ * \param res a matrix on the same ring as mat and has the same dimensions (empty)
+ * \param mat a polynomial matrix 
+ * \param degree the degree the user need to study 
+ *
+ */
 void coefficient_matrix(nmod_mat_t res, const nmod_poly_mat_t mat, int degree);
 
+/**
+ * \fn void column_degrees(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts)
+ * \brief Stock on res the columns degree of mat with the shifts shifts
+ * 
+ * \param res a pointer of an array of length the number of columns of mat (empty)
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of rows of mat   
+ *
+ */
 void column_degrees(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts);
 
+/**
+ * \fn void row_degrees(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts)
+ * \brief Stock on res the rows degree of mat with the shifts shifts
+ * 
+ * \param res a pointer of an array of length the number of columns of mat (empty)
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of columns of mat   
+ *
+ */
 void row_degrees(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts);
 
+/**
+ * \fn slong nmod_poly_mat_degree(const nmod_poly_mat_t mat)
+ * \brief Gives the maximal polynomial degree for all the entry of mat
+ * 
+ * \param mat a polynomial matrix
+ * \return the degree of the matrix mat
+ */
 slong nmod_poly_mat_degree(const nmod_poly_mat_t mat);
 
+
+/**
+ * \fn void degree_matrix(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts,
+                   matrix_wise row_wise)
+ * \brief Stock on res the degree matrix of mat with the shifts shifts see depending of the matrix_wise
+ * 
+ * \param res a pointer on the first value of the degree_matrix which being stock line by line (empty)
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of rows/columns of mat depending of row_wise   
+ * \param row_wise gives two options for the shifts
+ */
 void degree_matrix(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts,
                    matrix_wise row_wise);
 
 int is_hermite(const nmod_poly_mat_t mat, matrix_wise row_wise);
 
+/**
+ * \fn int is_popov(const nmod_poly_mat_t mat, uint64_t *shifts, matrix_wise row_wise, int ordered);
+ * \brief Verify if mat is  popov
+ * 
+ * Verify if mat is weak popov and the degree of each polynoms 
+ * are strictly inferior the pivot of the columns or rows
+ * 
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of rows/columns of mat depending of row_wise   
+ * \param row_wise gives two options for the shifts
+ * \param ordered, 1 if the leading positions must be increasing, 0 else
+ * \return 1 if mat is popov, 0 else
+ */
 int is_popov(const nmod_poly_mat_t mat, uint64_t *shifts, matrix_wise row_wise, int ordered);
 
+/**
+ * \fn  int is_reduced(const nmod_poly_mat_t mat, uint64_t *shifts, matrix_wise row_wise)
+ * \brief Verify if mat is reduced
+ * 
+ * Verify if the leading matrix is full rank (e.g for any unimodular polynomial matrix U, rdeg_s(mat) <= rdeg_s(U*mat) if row_wise == ROW_WISE)   
+ * 
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of rows/columns of mat depending of row_wise   
+ * \param row_wise gives two options for the shifts
+ * \return 1 if mat is reduced, 0 else
+ */
 int is_reduced(const nmod_poly_mat_t mat, uint64_t *shifts, matrix_wise row_wise);
 
+/**
+ * \fn int is_weak_popov(const nmod_poly_mat_t mat, uint64_t *shifts, matrix_wise row_wise, int ordered)
+ * \brief Verify if mat is weak popov
+ * 
+ * Verify if the leading positions of mat are distinct pairwise   
+ * 
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of rows/columns of mat depending of row_wise   
+ * \param row_wise gives two options for the shifts
+ * \param ordered, 1 if the leading positions must be increasing, 0 else
+ * \return 1 if mat is weak popov, 0 else
+ */
 int is_weak_popov(const nmod_poly_mat_t mat, uint64_t *shifts, matrix_wise row_wise, int ordered);
 
+/**
+ * \fn void leading_matrix(nmod_mat_t res, const nmod_poly_mat_t mat, uint64_t *shifts,
+                    matrix_wise row_wise)
+ * \brief Stock on res the leading matrix of mat with the shifts shifts see depending of the matrix_wise
+ * 
+ * Will use the row or column degrees of mat to compute the leading matrix of mat,
+ * For the i-st line, it will be stock the coefficient for the degree row/column_degrees[i] for the line.
+ * 
+ * \param res a nmod_mat_t where will be stock the result of dimensions mat (empty)
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of rows/columns of mat depending of row_wise   
+ * \param row_wise gives two options for the shifts
+ */
 void leading_matrix(nmod_mat_t res, const nmod_poly_mat_t mat, uint64_t *shifts,
                     matrix_wise row_wise);
 
+/**
+ * \fn void leading_positions(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts,
+                       matrix_wise row_wise)
+ * \brief Stock on res the leading positions of mat with the shifts shifts see depending of the matrix_wise
+ * 
+ * If we see mat with a ROW_WISE, the leading positions is the first column where the row degrees is reach starting by the right for each rows of mat.
+ *
+ * \param res a pointer with length the number of rows/columns of mat depending on row_wise (empty)
+ * \param mat a polynomial matrix 
+ * \param shifts a pointer of an array of lenght the number of columns/rows of mat depending of row_wise   
+ * \param row_wise gives two options for the shifts
+ */
 void leading_positions(uint64_t *res, const nmod_poly_mat_t mat, uint64_t *shifts,
                        matrix_wise row_wise);
 
