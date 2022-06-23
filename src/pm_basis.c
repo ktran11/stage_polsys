713bca7a8744ffbfae7d3eb0c516c6c06559ef1c
Tran Kevin <kevin.tran@etudiant.univ-rennes1.fr>
2022-06-23 16:21:29 +0200

    PM_basis second version

--- src/pm_basis.c
+++ src/pm_basis.c
@@ -3,7 +3,26 @@
 void middle_product(nmod_poly_mat_t res, const nmod_poly_mat_t A,
 		    const nmod_poly_mat_t B, slong d, slong h)
 {
+  slong rdim = A->r, cdim = B->c;
+  mp_limb_t prime = A->modulus;
+  nmod_poly_mat_t C;
+  nmod_poly_struct *P; 
+  nmod_poly_t R; 
   
+  nmod_poly_mat_init(C, rdim, cdim, prime);
+
+  nmod_poly_mat_mul(C, A, B);
+
+  nmod_poly_init(R, prime);
+  for (slong k = 0; k < rdim; k++)
+    for (slong l = 0; l < cdim; l++)
+      {
+	P = nmod_poly_mat_entry(C, k, l);
+	for (slong i = d - 1; i < h - d; i++)
+	  nmod_poly_set_coeff_ui(R, i - d + 1,
+				 nmod_poly_get_coeff_ui(P, i));
+	nmod_poly_set(P, R); 
+      }
 }
 
 
